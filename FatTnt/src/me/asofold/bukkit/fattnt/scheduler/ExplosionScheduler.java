d92596fd4cb23e620b02537ce10e69bbcc82069f
asofold <asofold@web.de>
2012-07-05 15:28:18 +0200

    adjust settings

--- FatTnt/src/me/asofold/bukkit/fattnt/scheduler/ExplosionScheduler.java
+++ FatTnt/src/me/asofold/bukkit/fattnt/scheduler/ExplosionScheduler.java
@@ -46,7 +46,7 @@ public final class ExplosionScheduler {
 		
 	}
 	
-	private int maxExplodeTotal = 10;
+	private int maxExplodeTotal = 20;
 	private int chunkSize = 16;
 	private int maxStoreTotal = 5000;
 	private int maxStoreChunk = 50; 
